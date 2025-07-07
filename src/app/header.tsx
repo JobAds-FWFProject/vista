4837717581fa0c537d616c6c300a5b6271cd2a5d
Gytha Ogg <gythaoggscat@gmail.com>
2025-07-07 23:46:28 +0200

    feat: better colours

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,36 +1,25 @@
 'use client';
+import './header.css';
 import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <nav className="max-w-4xl mx-auto flex gap-6 relative">
-        <Link href="/">Home</Link>
-        <Link href="/explore">Explore</Link>
+        <header>
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="/explore">Explore</Link>
+                <Link
+                    href="/wordtree"
+                >
+                    Word Tree
+                </Link>
+                <Link
+                    href="/network"
+                >
+                    Network
+                </Link>
 
-        <div className="relative group">
-          {/* Group-hover must apply to a shared hoverable box */}
-          <div className="cursor-pointer">Visualisations ▾</div>
-
-          {/* Wrapper that remains hoverable */}
-          <div className="absolute left-0 top-full w-max group-hover:block hidden">
-            <div className="bg-white text-black shadow-md rounded min-w-[12rem] z-10">
-              <Link
-                href="/wordtree"
-                className="block px-4 py-2 hover:bg-gray-100"
-              >
-                Word Tree
-              </Link>
-              <Link
-                href="/network"
-                className="block px-4 py-2 hover:bg-gray-100"
-              >
-                Network
-              </Link>
-            </div>
-          </div>
-        </div>
-      </nav>
-    </header>
-  );
+            </nav>
+        </header>
+    );
 }
